c53786e59ab9da623a52192028caa7166a7bf009
dand101 <100424460+dand101@users.noreply.github.com>
2023-12-08 18:28:13 +0200

    hands contr anim

---
+++ IMRProject/Assets/Scripts/HandScripts/AnimateHandOnInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimation;
+    public InputActionProperty gripAnimation;
+    public Animator handAnimator;
+
+    void Update()
+    {
+        float triggerValue = pinchAnimation.action.ReadValue<float>();
+        
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimation.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+
+    }
+}
